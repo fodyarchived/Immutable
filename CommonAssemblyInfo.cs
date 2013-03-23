89b98ba3261d7eb21a874070423e884258dbd8ea
Simon Cropp <simon.cropp@gmail.com>
2013-03-23 21:54:02 +1100

    move to fody nuget

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Immutable")]
 [assembly: AssemblyProduct("Immutable")]
-[assembly: AssemblyVersion("0.1.2.0")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
