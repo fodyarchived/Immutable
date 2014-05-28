a4c72ef6d53bd3ecfa763139b60f689ee71fac6e
Simon Cropp <simon.cropp@gmail.com>
2014-05-28 14:26:53 +1000

    better pcl support

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Immutable")]
 [assembly: AssemblyProduct("Immutable")]
-[assembly: AssemblyVersion("0.3.4")]
-[assembly: AssemblyFileVersion("0.3.4")]
+[assembly: AssemblyVersion("0.3.5")]
+[assembly: AssemblyFileVersion("0.3.5")]
